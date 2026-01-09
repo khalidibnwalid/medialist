96326b4f8a5c29a0273c1735cfb40e55297b16d9
khalidibnwalid <kh.hawajreh@gmail.com>
2026-01-09 12:12:39 +0300

    perf: configure sharp cache settings we had memory usage spiking (to ~500mg) I did disable cache `(sharp.cache(false)`, it fixed the memory usege, but caused the cpu to spike at 450% so for a middle ground I just added strict limits for the cache

--- src/server/utils/lib/webpTransformer.ts
+++ src/server/utils/lib/webpTransformer.ts
@@ -1,8 +1,15 @@
-import sharp from "sharp"
+import sharp from "sharp";
 
-export const $webpTransformer = (w?: number, h?: number) => sharp()
+sharp.cache({
+  memory: 50,
+  items: 100,
+  files: 20,
+});
+
+export const $webpTransformer = (w?: number, h?: number) =>
+  sharp()
     .resize(w, h)
     .webp()
-    .on('error', error => {
-        throw new Error(`Failed to convert image: ${error}`)
-    })
+    .on("error", (error) => {
+      throw new Error(`Failed to convert image: ${error}`);
+    });
